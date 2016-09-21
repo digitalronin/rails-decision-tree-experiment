3402c723b7e4add01c8e261edc1193c91f091653
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-21 15:41:12 +0100

    refactor routes
    Define questions as a list of strings;
    age, income, marital_status

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :age_question
-  resources :marital_status_question
-  resources :income_question
+  %w(
+    age
+    marital_status
+    income
+  ).each do |question|
+    resources "#{question}_question".to_sym
+  end
   resources :decision
 end
