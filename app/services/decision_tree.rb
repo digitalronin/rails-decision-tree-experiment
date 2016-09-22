d49661036c19f48dfa99cbdca03297fd594926fb
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-22 12:36:22 +0100

    simplify the routes file
    For some reason, removing the _question suffix
    from the controllers causes rails to throw
    template format errors, so leave it

--- app/services/decision_tree.rb
+++ app/services/decision_tree.rb
@@ -59,6 +59,7 @@ class DecisionTree
   #----------------------
 
   def goto_question(q)
+    # TODO: remove hard-coded 'localhost:3000' - use request.host_with_port instead
     url_for host: "localhost:3000", action: :edit, controller: ["questions/", q, "_question"].join, id: @object
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :questions do
-    %w(
-      age
-      marital_status
-      income
-    ).each do |question|
-      resources "#{question}_question".to_sym, only: [:edit, :update]
+    [
+      :age_question,
+      :marital_status_question,
+      :income_question
+    ].each do |ctrlr|
+      resources ctrlr, only: [:edit, :update]
     end
   end
 
