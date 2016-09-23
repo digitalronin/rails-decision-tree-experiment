2a274724d4ab6a4b08f7cbc760db0bb6dc8bf874
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-23 11:26:24 +0100

    add annotate gem

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id             :integer          not null, primary key
+#  age            :string
+#  income         :string
+#  marital_status :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
 class Person < ApplicationRecord
 end
