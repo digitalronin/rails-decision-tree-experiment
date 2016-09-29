9f3a6d368e91c1aad7772ff7afebc2adeadc2240
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-29 16:19:01 +0100

    WiP: generalise the form object approach

--- app/controllers/step_controller.rb
+++ app/controllers/step_controller.rb
@@ -22,4 +22,24 @@ class StepController < ApplicationController
 
     redirect_to DecisionTree.new(object: person, step: hash, next_step: next_step).destination
   end
+
+  def update_and_advance2(attr, form_class, opts = {})
+    person = Person.find params.fetch(:id)
+    hash = params.fetch(:form_object, {})
+    @form_object = form_class.new hash.merge(person: person)
+    @next_step = params[:next_step].blank? ? false : params[:next_step]
+
+    if @form_object.save
+      # if we are reusing a step (e.g. income, income2, income3)
+      # we need to rename the 'income' attribute to 'income2' (or whatever) for the
+      # DecisionTree#destination call
+      # i.e. { income: "high" } -> { income2: "high" }
+      hash = opts[:as] ? { opts[:as] => hash[attr] } : hash
+
+      # TODO: do we have the updated person, via @form_object.person?
+      redirect_to DecisionTree.new(object: person, step: hash, next_step: @next_step).destination
+    else
+      render :edit
+    end
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,6 +11,20 @@ module ApplicationHelper
     end
   end
 
+  # Render a form_for tag pointing to the update action of the current
+  # controller, with a hidden field for any 'next_step' that was passed in
+  def step_form2(record, options = {}, &block)
+    opts = {
+      url: { controller: controller.controller_path, action: :update },
+      method: :put,
+      as: :form_object
+    }.merge(options)
+
+    form_for record, opts do |f|
+      hidden_field_tag(:next_step, @next_step) + capture { yield f }
+    end
+  end
+
   def step_path(step, object, opts = {})
     params = {
       # TODO: remove hard-coded 'localhost:3000' - use request.host_with_port instead
